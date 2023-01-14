771ca25fbfeaaed0e31e27a609cdb4378b779e91
Alex Decesare <adecesa1@my.westga.edu>
2023-01-14 17:21:22 -0500

    Add project files.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,15 @@
+using BlazorApp3.Client;
+using BlazorApp3.Client.Pages;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Web.WebView2;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
